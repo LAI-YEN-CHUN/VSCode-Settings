7f63887ad692f51feda4516148f6138d4159c9d4
LAI-YEN-CHUN <73031744+LAI-YEN-CHUN@users.noreply.github.com>
2025-11-27 07:43:38 +0000

    chore: auto-format with Ruff

--- src/ruff-action-ci-test/ruff-action-ci-test.py
+++ src/ruff-action-ci-test/ruff-action-ci-test.py
@@ -1,20 +1,16 @@
-import sys
 import numpy as np
-import os
+
+
 def add_arrays(arr1, arr2):
     return np.add(arr1, arr2)
+
+
 def multiply_arrays(arr1, arr2):
     return np.multiply(arr1, arr2)
 
 
-
-
-
 if __name__ == "__main__":
-    a = np.array([1, 2,     3]) #Test comment with trailing spaces
-    b =   np.array([4, 5,6]) #  Another comment
+    a = np.array([1, 2, 3])  # Test comment with trailing spaces
+    b = np.array([4, 5, 6])  #  Another comment
     print("Addition:", add_arrays(a, b))
     print("Multiplication:", multiply_arrays(a, b))
-
-
-
