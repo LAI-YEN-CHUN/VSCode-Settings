a3633f5a7719d6f2395990eab4ada45827f926b9
LAI-YEN-CHUN <terumo910311@gmail.com>
2025-11-27 16:52:47 +0800

    Integrate Python Pyright type-check workflow

---
+++ src/pyright-action-ci-test/pyright-action-ci-test.py
@@ -0,0 +1,12 @@
+def type_check_case_1(val1: int, val2: int) -> int:
+    return val1 + val2
+
+
+def type_check_case_2(val1: int, val2: int) -> str:
+    return val1 + val2
+
+
+if __name__ == "__main__":
+    print(type_check_case_1(5, 10))
+    print(type_check_case_2(5, 10))
+    print(type_check_case_1("5", "10"))
