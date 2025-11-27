c1aae4aa81f16d4cff3b2256899ed19392ae2a39
LAI-YEN-CHUN <terumo910311@gmail.com>
2025-11-27 16:58:17 +0800

    Fix test function return type and improve workflow paths

--- src/pyright-action-ci-test/pyright-action-ci-test.py
+++ src/pyright-action-ci-test/pyright-action-ci-test.py
@@ -2,11 +2,11 @@ def type_check_case_1(val1: int, val2: int) -> int:
     return val1 + val2
 
 
-def type_check_case_2(val1: int, val2: int) -> str:
+def type_check_case_2(val1: int, val2: int) -> int:
     return val1 + val2
 
 
 if __name__ == "__main__":
     print(type_check_case_1(5, 10))
     print(type_check_case_2(5, 10))
-    print(type_check_case_1("5", "10"))
+    # print(type_check_case_1("5", "10"))
